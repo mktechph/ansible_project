5aa89a9d64a282b7894b735a5ad8f3e31be9467f
mktechph <114472493+mktechph@users.noreply.github.com>
2023-06-23 21:59:33 +0800

    Add files via upload

---
+++ html/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Page</title>
+    <style>
+        body {
+            background-color: #f1f1f1;
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+
+        h1 {
+            color: #333333;
+            margin-top: 150px;
+        }
+
+        p {
+            color: #666666;
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello! Welcome!</h1>
+    <p>This server is <?php echo gethostname(); ?></p>
+</body>
+</html>
